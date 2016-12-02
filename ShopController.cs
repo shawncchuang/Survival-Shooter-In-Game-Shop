70d11157b3786290f7b84f2a87d206a60c4da638
Shawn Huang <shawncc.huang@gmail.com>
2016-12-02 19:01:01 +0800

    Survival Shooter In-Game Shop

---
+++ ShopController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShopController : MonoBehaviour
+{
+
+	public Canvas shopCanvas;
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.CompareTag ("Player")) {
+			OpenShop ();
+		}
+	}
+
+	void OpenShop ()
+	{
+		shopCanvas.enabled = true;
+		Time.timeScale = 0;
+
+	}
+
+	public void CloseShop ()
+	{
+		shopCanvas.enabled = false;
+		Time.timeScale = 1;
+
+	}
+	 
+}
---
+++ WeaponButton.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.ComponentModel;
+
+namespace CompleteProject
+{
+	public class WeaponButton : MonoBehaviour
+	{
+
+		public PlayerShooting playerShooting;
+		public int weaponNumber;
+
+		public Text name;
+		public Text cost;
+		public Text desription;
+
+		private AudioSource source;
+
+		// Use this for initialization
+		void Start ()
+		{
+			//source.GetComponent<AudioSource> ();
+			SetButton ();
+		}
+
+		void SetButton ()
+		{
+			string costString = playerShooting.weapons [weaponNumber].cost.ToString ();
+			name.text = playerShooting.weapons [weaponNumber].weaponName;
+			cost.text = "$" + playerShooting.weapons [weaponNumber].cost;
+			desription.text = playerShooting.weapons [weaponNumber].description;
+		}
+
+		public void onClick ()
+		{
+			if (ScoreManager.score >= playerShooting.weapons [weaponNumber].cost) {
+				ScoreManager.score -= playerShooting.weapons [weaponNumber].cost;
+				playerShooting.currentWeapon = weaponNumber;
+			} else {
+
+//				source.Play ();
+
+			}
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+	}
+
+}
+
+
+
